a6c9835106b1f43e65fab8943ee38d7db0b51ab4
RubberDuck01 <spehar.luka@hotmail.com>
2023-10-01 14:20:02 +0200

    minor update

--- Python/LM_WeightReset.py
+++ Python/LM_WeightReset.py
@@ -14,6 +14,7 @@ try:
     #? closest values to dead-center of the weight triangle:
     new_weight = [ 0.333299994468689, 0.333299994468689, 0.333299994468689 ]
     json_files = [file for file in os.listdir() if file.endswith(".json")]
+    json_cntr = 0
 
     for json_file in json_files:
         with open(json_file, "r") as file:
@@ -21,15 +22,16 @@ try:
 
         if "Weight" in data:
             data["Weight"] = new_weight
-            print(f" -> Modified weight for '{json_file}'.")
+            print(f"Modified weight for '{json_file}'")
+            json_cntr += 1
         else:
-            print(f"The file '{json_file}' doesn't contain 'Weight' field! Skipping it...")
+            print(f"Couldn't modify '{json_file}' as it doesn't contain 'Weight' field. Skipping it...")
 
         #* write new json:
         with open(json_file, "w") as file:
             json.dump(data, file, indent = 4)
 
-    print("JSONs successfully modified!")
+    print(f"\nFinished!\nSuccessfully modified {json_cntr} of {len(json_files)} files.")
     input("Press any key to continue...")
 
 except Exception as e:
