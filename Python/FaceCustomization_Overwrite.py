727adb595deecad7f8125350fe67104b88b6ef06
RubberDuck01 <spehar.luka@hotmail.com>
2024-01-14 15:36:47 +0100

    Update 2, this should work

--- Python/FaceCustomization_Overwrite.py
+++ Python/FaceCustomization_Overwrite.py
@@ -2,17 +2,17 @@ import os
 import shutil
 
 def overwrite_textures(src_dir, dst_dir):
-    my_textures = [f for f in os.listdir(src_dir) if f.endswith(('_d.dds', '_n.dds', '_s.dds'))]
+    texture_types = ['_d.dds', '_n.dds', '_s.dds']
     
     for subdir, _, files in os.walk(dst_dir):
         for file in files:
-            if file.endswith(('_d.dds', '_n.dds', '_s.dds')):
-                my_texture = next((t for t in my_textures if file.startswith(t[:-6])), None)
-                if my_texture:
-                    my_texture_path = os.path.join(src_dir, my_texture)
+            for texture_type in texture_types:
+                if file.endswith(texture_type):
+                    master_texture = 'basefemalehead' + texture_type
+                    master_texture_path = os.path.join(src_dir, master_texture)
                     file_path = os.path.join(subdir, file)
-                    shutil.copy(my_texture_path, file_path)
-                    print(f'Replaced {my_texture_path} with {file_path}')
+                    shutil.copyfile(master_texture_path, file_path)
+                    print(f'Replaced {file} with {master_texture} in {subdir}')
                     
 my_textures_dir = 'MyTextures'
 face_customization_dir = 'FaceCustomization'
